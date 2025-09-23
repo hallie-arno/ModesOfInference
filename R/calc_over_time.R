b88f91fdbcb8be729ae405e02512acc7b77ef88e
Alec L. Robitaille <robit.alec@gmail.com>
2025-09-23 12:01:37 -0300

    calc over time function

---
+++ R/calc_over_time.R
@@ -0,0 +1,20 @@
+calc_over_time <- function(DT) {
+  DT %>%
+    filter(Journal != "") %>%
+    mutate(Frequentist = if_else(Type == "Frequentist", TRUE, Frequentist)) %>%
+    mutate(Evidentialist = if_else(Type == "Evidentialist", TRUE, Evidentialist)) %>%
+    filter(Title != "Social ontogeny in the communication system of an insect") %>%
+    #  filter(Journal %in% c("AnBehav", "CJFAS", "LandEcol")) %>%
+    group_by(Year) %>%
+    #filter(Evidentialist != TRUE & Frequentist != TRUE & AIC != TRUE & Bayesian !=TRUE) %>%
+    summarize(AIC = sum(AIC, na.rm = TRUE),
+              Bayesian = sum(Bayesian, na.rm = TRUE),
+              Frequentist = sum(Frequentist, na.rm = TRUE),
+              Evidentialist = sum(Evidentialist, na.rm = TRUE)) %>%
+    ungroup() %>%
+    rowwise() %>%
+    mutate(Total = sum(AIC, Bayesian, Frequentist, Evidentialist)) %>%
+    # filter(Journal == "AnBehav" & Year == 2019) %>%
+    pivot_longer(cols = c(AIC, Bayesian, Frequentist, Evidentialist)) %>%
+    mutate(across(name, ~factor(., levels=c("Frequentist","AIC", "Evidentialist","Bayesian"))))
+}
